3780ab5cabbbbc808c1fab2296cc8122a8de5253
StratocasterO <omarolmedoferrer@gmail.com>
2019-09-13 12:50:52 +0200

    register screen

--- app/src/main/java/com/omar/openhuts/Favorites.java
+++ app/src/main/java/com/omar/openhuts/Favorites.java
@@ -2,7 +2,10 @@ package com.omar.openhuts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class Favorites extends AppCompatActivity {
 
@@ -11,4 +14,9 @@ public class Favorites extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_favorites);
 	}
+	public void menu(View v) {
+		Log.d("click", "clicked on menu");
+		startActivity(new Intent(this, MenuApp.class));
+	}
+
 }
