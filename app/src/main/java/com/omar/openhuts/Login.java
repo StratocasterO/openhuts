f32bf8e473db40643e88eba2034d8055b2fe7d0e
StratocasterO <omarolmedoferrer@gmail.com>
2019-09-13 09:54:51 +0200

    cambiado el menu por un activity

---
+++ app/src/main/java/com/omar/openhuts/Login.java
@@ -0,0 +1,22 @@
+package com.omar.openhuts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+	public void menu(View v) {
+		Log.d("click", "clicked on menu");
+		startActivity(new Intent(this, MenuApp.class));
+	}
+}
--- app/src/main/java/com/omar/openhuts/MainActivity.java
+++ app/src/main/java/com/omar/openhuts/MainActivity.java
@@ -4,31 +4,67 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.content.Context;
-import android.location.LocationManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.omar.openhuts.R;
+
+import static com.omar.openhuts.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+	View mapView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		SupportMapFragment mapFragment  = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+		ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+		ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
+		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+		mapView = mapFragment.getView();
 		assert mapFragment != null;
 		mapFragment.getMapAsync(this);
 	}
 
+	public void menu(View v) {
+		Log.d("click", "clicked on menu");
+		startActivity(new Intent(this, MenuApp.class));
+	}
+
+	public void search(View v) {
+		Log.d("click", "clicked on search");
+	}
+
+	public void add(View v) {
+		Log.d("click", "clicked on add");
+	}
+
 	@Override
 	public void onMapReady(GoogleMap googleMap) {
-		ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-		ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
-
 		googleMap.setMyLocationEnabled(true);
+
+		if (mapView != null &&
+				mapView.findViewById(Integer.parseInt("1")) != null) {
+			// Get the button view
+			View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+			// and next place it, on bottom right (as Google Maps app)
+			RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)
+					locationButton.getLayoutParams();
+			// position on right bottom
+			layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+			layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+			layoutParams.setMargins(0, 0, 8 , 8);
+		}
 	}
 }
