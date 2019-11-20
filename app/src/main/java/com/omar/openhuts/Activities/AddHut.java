5281ca43f5d7202c5806442d92144b2b608e9308
StratocasterO <omarolmedoferrer@gmail.com>
2019-11-20 14:27:55 +0100

    imagenes odenadas en carpetas, añadidas imagenes de servicios

--- app/src/main/java/com/omar/openhuts/Activities/AddHut.java
+++ app/src/main/java/com/omar/openhuts/Activities/AddHut.java
@@ -16,5 +16,10 @@ public class AddHut extends DefaultActivity {
 		tv.setText("Hut");
 	}
 
+
+
 	// TODO https://pablomonteserin.com/curso/android/ "Subida de una imagen de la galería al servidor con Retrofit"
+	// https://rstopup.com/subir-la-imagen-a-amazon-s3-mediante-multer-s3-nodejs.html
+	// https://codeday.me/es/qa/20190131/156499.html
+	// https://stackabuse.com/uploading-files-to-aws-s3-with-node-js/
 }
